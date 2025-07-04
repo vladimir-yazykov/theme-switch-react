38926f6a8e08547280d468a791a8ff642aa25285
Vladimir Yazykov <neizerth@gmail.com>
2025-07-04 12:22:01 +0300

    feat: theme switch

---
+++ src/components/ThemeSwitch/ThemeSwitch.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+import "./ThemeSwitch.css";
+
+export const ThemeSwitch = () => {
+  const [currentTheme, setCurrentTheme] = useState("light");
+  const onThemeSwitch = (theme) => {
+    console.log(theme);
+    setCurrentTheme(theme);
+  };
+  return (
+    <div className="theme-switch">
+      <div className="theme-switch__buttons">
+        <button
+          onClick={() => onThemeSwitch("dark")}
+          className={
+            currentTheme === "dark"
+              ? "theme-switch__button theme-switch__button_active"
+              : "theme-switch__button"
+          }
+        >
+          🌑Dark
+        </button>
+        <button
+          onClick={() => onThemeSwitch("light")}
+          className={
+            currentTheme === "light"
+              ? "theme-switch__button theme-switch__button_active"
+              : "theme-switch__button"
+          }
+        >
+          ☀️Light
+        </button>
+      </div>
+      <div className="theme-switch__label">{currentTheme}</div>
+    </div>
+  );
+};
